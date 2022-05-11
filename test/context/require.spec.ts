6762454b47314baf9239c60c1ce5ac70831180fa
Joshua Honig <joshua@mallowfields.com>
2022-05-10 21:53:43 -0400

    Add missing license headers

--- test/context/require.spec.ts
+++ test/context/require.spec.ts
@@ -1,3 +1,7 @@
+// Copyright 2022 the Sabl authors. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 import { Context, IContext } from '$';
 
 const ctxKeyString = Symbol('string');
--- test/fixtures/custom-context.ts
+++ test/fixtures/custom-context.ts
@@ -1,3 +1,7 @@
+// Copyright 2022 the Sabl authors. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 import { Canceler } from '$';
 import { IContext } from '$/context';
 
