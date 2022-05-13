b7426c11fac9d5faf49f5a4d287c270066a07619
Joshua Honig <joshua@mallowfields.com>
2022-05-13 11:45:07 -0400

    build: repo and tooling initialization
    Adds all configuration files for repository, package, and development
    tooling.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^\\$$': path.join(__dirname, 'src'),
+    '^\\$/(.*)$': path.join(__dirname, 'src/$1'),
+    '^\\$test/(.*)$': path.join(__dirname, 'test/$1'),
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: path.join(__dirname, 'coverage'),
+  coverageReporters: ['text'],
+};
