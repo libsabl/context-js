dcf218cda0ed2043f2e862e5c3887760f46b6387
Joshua Honig <jmhonetech@gmail.com>
2022-07-21 21:53:46 -0400

    build: clean up package dependencies

--- build/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  testMatch: ['**/*.spec.*'],
-  coverageReporters: ['text'],
-};
